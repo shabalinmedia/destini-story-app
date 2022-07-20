014153f2fa348d741841a7940ed3a9f88bf1c63e
Alex Shabalin <shabalin.media@gmail.com>
2022-07-20 23:53:59 +0600

    Need to add story.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -1,5 +1,3 @@
-
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -8,11 +6,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        storyBrain.nextStory(userAnswer)
+        updateUI()
 
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getQestionText()
+        choice1Button.setTitle(storyBrain.example[storyBrain.questionNumber].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.example[storyBrain.questionNumber].choice2, for: .normal)
+    }
+    
+    
+    
+    
 }
 
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -1,5 +1,35 @@
-
-
 import Foundation
+
+struct StoryBrain {
+    let example = [Story(title: "You see a fork in the road.",
+    choice1: "Take a left.",
+    choice2: "Take a right."),
+                   Story(title: "You see a tiger.",
+                   choice1: "Shout for help.",
+                   choice2: "Play dead."),
+                   Story(title: "You find a treasure chest.",
+                   choice1: "Open it.",
+                   choice2: "Check for traps."),
+    ]
+    
+    var questionNumber = 0
+    
+    mutating func nextStory(_ userAnswer: String) {
+        if userAnswer == example[questionNumber].choice1 {
+            questionNumber += 1
+          
+
+        } else if userAnswer == example[questionNumber].choice2 {
+            questionNumber += 2
+        }
+        
+    }
+    
+    func getQestionText () -> String {
+        return example[questionNumber].title
+    }
+    
+    
+}
 
 
